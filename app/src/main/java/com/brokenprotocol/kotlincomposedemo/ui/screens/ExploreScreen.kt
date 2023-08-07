e1b6b5a76a056abed8abb8e0f2ffb0408150773d
Beech316 <beech316@gmail.com>
2023-08-07 00:46:50 -0700

    Updated fonts, added details row, built new detail cell

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/ExploreScreen.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/ExploreScreen.kt
@@ -1,23 +1,33 @@
 package com.brokenprotocol.kotlincomposedemo.ui.screens
 
+import android.text.style.AlignmentSpan
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,9 +45,18 @@ fun ExploreScreen(
     val list = DataManager.getCategoryList()
 
     Column() {
-        Text(text = "Categories")
+
+        Text(
+            text = stringResource(id = R.string.categories_header_title),
+            modifier = Modifier.padding(horizontal = dimens.medium, vertical = dimens.small),
+            fontSize = dimens.exploreSectionHeader
+        )
+
         LazyRow(
-            contentPadding = PaddingValues(horizontal = dimens.small, vertical = dimens.small)
+            contentPadding = PaddingValues(horizontal = dimens.small, vertical = dimens.none),
+            modifier = Modifier
+                .wrapContentHeight()
+                .wrapContentWidth()
         ) {
             items(
                 list.size,
@@ -48,7 +67,40 @@ fun ExploreScreen(
                     modifier = Modifier
                         .padding(
                             all = dimens.small
-                        ).selectable(
+                        )
+                        .selectable(
+                            selected = false,
+                            onClick = {
+
+                            }
+                        )
+                )
+            }
+        }
+
+        Text(
+            text = stringResource(id = R.string.details_header_title),
+            modifier = Modifier.padding(horizontal = dimens.medium, vertical = dimens.small),
+            fontSize = dimens.exploreSectionHeader
+        )
+
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = dimens.small, vertical = dimens.none),
+            modifier = Modifier
+                .wrapContentHeight()
+                .wrapContentWidth()
+        ) {
+            items(
+                list.size,
+                key = { list[it].id },
+            ) {
+                DetailListItem(
+                    category = list[it],
+                    modifier = Modifier
+                        .padding(
+                            all = dimens.small
+                        )
+                        .selectable(
                             selected = false,
                             onClick = {
 
@@ -58,8 +110,6 @@ fun ExploreScreen(
             }
         }
     }
-
-
 }
 
 @Composable
@@ -73,10 +123,13 @@ fun CategoryListItem(
         elevation = dimens.extraSmall,
         shape = RoundedCornerShape(dimens.medium),
         modifier = modifier
+            .height(150.dp)
+            .width(150.dp)
     ) {
         Box(
             contentAlignment = Alignment.Center,
-        ){
+            modifier = Modifier.fillMaxSize()
+        ) {
 
             if (category.imageUrlString.isNotEmpty()) {
                 AsyncImage(
@@ -84,22 +137,85 @@ fun CategoryListItem(
                     contentDescription = "Category image",
                     contentScale = ContentScale.FillBounds,
                     modifier = Modifier
-                        .height(150.dp)
-                        .width(150.dp)
                         .align(Alignment.Center)
                 )
             } else {
                 Image(
                     painter = painterResource(id = R.drawable.category_image),
-                    contentDescription = "Default Image")
+                    contentDescription = "Default Image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
 
             Text(
                 text = category.name,
                 color = Color.White,
-                fontSize = 32.sp,
+                fontSize = dimens.categoryItemTitle,
                 textAlign = TextAlign.Center
             )
+        }
+    }
+}
+
+@Composable
+fun DetailListItem(
+    category: Category,
+    modifier : Modifier = Modifier)
+{
+    val dimens = LocalDimension.current
+
+    Card(
+        elevation = dimens.extraSmall,
+        shape = RoundedCornerShape(dimens.medium),
+        modifier = modifier
+            .height(200.dp)
+            .width(240.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            if (category.imageUrlString.isNotEmpty()) {
+                AsyncImage(
+                    model = category.imageUrlString,
+                    contentDescription = "Category image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.category_image),
+                    contentDescription = "Default Image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.25f)
+                    .align(Alignment.BottomCenter)
+            ) {
+                Surface(
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f)
+                ) {}
+
+                Text(
+                    text = category.name,
+                    color = Color.White,
+                    fontSize = dimens.categoryItemTitle,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/LocalDimensions.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/LocalDimensions.kt
@@ -2,19 +2,26 @@ package com.brokenprotocol.kotlincomposedemo.ui.theme
 
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 // Future upgrade: https://proandroiddev.com/supporting-different-screen-sizes-on-android-with-jetpack-compose-f215c13081bd
 
 val LocalDimension = compositionLocalOf { Dimensions() }
 
-data class Dimensions(
-    val default: Dp = 0.dp,
-    val extraExtraSmall: Dp = 2.dp,
-    val extraSmall: Dp = 4.dp,
-    val small: Dp = 8.dp,
-    val medium: Dp = 16.dp,
-    val large: Dp = 32.dp,
-    val extraLarge: Dp = 64.dp,
-    val extraExtraLarge: Dp = 128.dp
+data class Dimensions (
+    val default : Dp = 8.dp,
+    val none : Dp = 0.dp,
+    val extraExtraSmall : Dp = 2.dp,
+    val extraSmall : Dp = 4.dp,
+    val small : Dp = 8.dp,
+    val medium : Dp = 16.dp,
+    val large : Dp = 32.dp,
+    val extraLarge : Dp = 64.dp,
+    val extraExtraLarge : Dp = 128.dp,
+
+    val exploreSectionHeader : TextUnit = 32.sp,
+
+    val categoryItemTitle : TextUnit = 24.sp
 )
