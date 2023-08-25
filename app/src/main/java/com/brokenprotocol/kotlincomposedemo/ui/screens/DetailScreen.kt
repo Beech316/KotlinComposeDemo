3187894fe33c89965415774083c237c448d66a57
Beech316 <beech316@gmail.com>
2023-08-24 19:22:32 -0700

    Added FAB to detail

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/DetailScreen.kt
@@ -11,16 +11,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -50,60 +56,81 @@ fun DetailScreen(
     }
     val shareIntent = Intent.createChooser(sendIntent, null)
 
-    Column() {
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.height(200.dp)
+    Scaffold(
+        floatingActionButton = { FloatingActionButtonCompose() }
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
         ) {
-            LoadImageOrDefault(
-                imageUrl = images[it],
-                default = R.drawable.detail_image
-            )
-        }
-
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-        ) {
-            IconButton(
-                onClick = {
-                    context.startActivity(shareIntent)
-                }
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.height(200.dp)
             ) {
-                Icon(
-                    imageVector = Icons.Outlined.Share,
-                    contentDescription = null
+                LoadImageOrDefault(
+                    imageUrl = images[it],
+                    default = R.drawable.detail_image
                 )
             }
 
-            Text(
-                text = detail.name,
-                modifier = Modifier.weight(1f).align(Alignment.CenterVertically),
-                textAlign = TextAlign.Center
-            )
-
-            IconButton(
-                onClick = { onLikeSelected() }
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
             ) {
-                if (liked) {
+                IconButton(
+                    onClick = {
+                        context.startActivity(shareIntent)
+                    }
+                ) {
                     Icon(
-                        imageVector = Icons.Outlined.Favorite,
-                        contentDescription = null
-                    )
-                } else {
-                    Icon(
-                        imageVector = Icons.Outlined.FavoriteBorder,
+                        imageVector = Icons.Outlined.Share,
                         contentDescription = null
                     )
                 }
-            }
-        }
 
-        Text(
-            text = detail.desc,
-            modifier = Modifier.padding(dimens.small)
-        )
+                Text(
+                    text = detail.name,
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+
+                IconButton(
+                    onClick = { onLikeSelected() }
+                ) {
+                    if (liked) {
+                        Icon(
+                            imageVector = Icons.Outlined.Favorite,
+                            contentDescription = null
+                        )
+                    } else {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+
+            Text(
+                text = detail.desc,
+                modifier = Modifier.padding(dimens.small)
+            )
+
+        }
 
     }
 
+}
+
+@Composable
+fun FloatingActionButtonCompose() {
+    val context = LocalContext.current
+    FloatingActionButton(shape = MaterialTheme.shapes.large.copy(CornerSize(percent = 50)),
+        contentColor = Color.White,
+        onClick = {
+            Log.i("DetailScreen", "FAB Click")
+        }) {
+        Icon(Icons.Default.Add, contentDescription = null)
+    }
 }
