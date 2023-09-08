56bb50f49d2238f2bc3a4f1dcab40f953201192b
Beech316 <beech316@gmail.com>
2023-09-08 10:31:10 -0700

    Added divider and email login

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginSplashScreen.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginSplashScreen.kt
@@ -1,12 +1,9 @@
 package com.brokenprotocol.kotlincomposedemo.ui.screens.login
 
-import android.content.Intent
-import android.net.Uri
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -16,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.brokenprotocol.kotlincomposedemo.ui.theme.LocalDimension
-import java.util.Locale
 
 @Composable
 fun LoginSplashScreen(
@@ -24,70 +20,96 @@ fun LoginSplashScreen(
 ) {
     Column(
         modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
     ) {
 
         val dimens = LocalDimension.current
         val context = LocalContext.current
 
-        Button(
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = dimens.small),
-            onClick = {
-
-            },
-            colors = ButtonDefaults.buttonColors(Color.White)
+                .padding(start = dimens.none, top = dimens.small, end = dimens.none, bottom = dimens.small)
+                .fillMaxWidth()
         ) {
-            Text(text = "Apple")
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(Color.White)
+            ) {
+                Text(text = "Apple")
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(Color.LightGray)
+            ) {
+                Text(text = "Google")
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(Color.Blue)
+            ) {
+                Text(text = "Facebook")
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(Color.Yellow)
+            ) {
+                Text(text = "X / Twitter")
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(Color.Magenta)
+            ) {
+                Text(text = "Github")
+            }
         }
 
-        Button(
+        Divider(modifier = Modifier.padding(start = dimens.large, top = dimens.none, end = dimens.large, bottom = dimens.none), thickness = dimens.extraExtraSmall)
+
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = dimens.small),
-            onClick = {
-
-            },
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
+                .padding(top = dimens.small)
+                .fillMaxWidth()
         ) {
-            Text(text = "Google")
-        }
+            Button(
+                modifier = Modifier
+                    .padding(top = dimens.small)
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
 
-        Button(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = dimens.small),
-            onClick = {
+                },
+                colors = ButtonDefaults.buttonColors(Color.White)
+            ) {
+                Text(text = "Email / Password")
+            }
 
-            },
-            colors = ButtonDefaults.buttonColors(Color.Blue)
-        ) {
-            Text(text = "Facebook")
-        }
-
-        Button(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = dimens.small),
-            onClick = {
-
-            },
-            colors = ButtonDefaults.buttonColors(Color.Yellow)
-        ) {
-            Text(text = "X / Twitter")
-        }
-
-        Button(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = dimens.small),
-            onClick = {
-
-            },
-            colors = ButtonDefaults.buttonColors(Color.Magenta)
-        ) {
-            Text(text = "Github")
         }
 
     }
