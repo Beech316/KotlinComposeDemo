b277b5fa5ea1ea4570b90110ad36a43a107a0ca4
Beech316 <beech316@gmail.com>
2023-09-18 12:25:54 -0700

    Added text entry fields to email/pass screen

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginEmailScreen.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginEmailScreen.kt
@@ -1,17 +1,59 @@
 package com.brokenprotocol.kotlincomposedemo.ui.screens.login
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoginEmailScreen(
     modifier : Modifier = Modifier,
 ) {
-    Box(modifier = Modifier.height(100.dp).width(100.dp).background(Color.Red))
+
+    var emailText by remember { mutableStateOf("") }
+    var passwordText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        Spacer(modifier = Modifier.weight(1.0f))
+
+        Column(modifier = modifier
+            .fillMaxWidth()
+            .align(Alignment.CenterHorizontally)
+            .wrapContentHeight()
+        ) {
+
+            OutlinedTextField(
+                value = emailText,
+                onValueChange = { emailText = it },
+                label = { Text(text = "Email") }
+            )
+
+            OutlinedTextField(
+                value = passwordText,
+                onValueChange = { passwordText = it },
+                label = { Text(text = "Password") }
+            )
+
+        }
+
+        Spacer(modifier = Modifier.weight(1.0f))
+
+    }
+
 }
