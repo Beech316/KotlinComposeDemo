0c80bf4854c4585544bbfcde1b5d38e2b3155481
Beech316 <beech316@gmail.com>
2023-10-19 14:05:52 -0700

    Toyed around with themes and colours

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/Color.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Red500 = Color(0xFFFA2915)
+val Red800 = Color(0xFFCD0009)
+val Yellow500 = Color(0xFFFAF215)
--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/Theme.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/theme/Theme.kt
@@ -16,17 +16,20 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Red800,
+    secondary = Red500,
+    tertiary = Yellow500,
+    primaryContainer = Red500
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Red500,
+    secondary = Red800,
+    tertiary = Yellow500,
+    primaryContainer = Red500
+)
 
-    /* Other default colors to override
+/* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -35,7 +38,6 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
 
 @Composable
 fun KotlinComposeDemoTheme(
@@ -44,15 +46,8 @@ fun KotlinComposeDemoTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
