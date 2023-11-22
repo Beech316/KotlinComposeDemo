eb6b96a9ddad4f7996e6f912c8e0b563c963909e
Beech316 <beech316@gmail.com>
2023-11-21 16:29:42 -0800

    Added pin input and centred

--- app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginPasscodeScreen.kt
+++ app/src/main/java/com/brokenprotocol/kotlincomposedemo/ui/screens/login/LoginPasscodeScreen.kt
@@ -1,11 +1,16 @@
 package com.brokenprotocol.kotlincomposedemo.ui.screens.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -16,7 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import com.brokenprotocol.kotlincomposedemo.ui.theme.LocalDimension
+import com.composeuisuite.ohteepee.OhTeePeeInput
+import com.composeuisuite.ohteepee.configuration.OhTeePeeCellConfiguration
+import com.composeuisuite.ohteepee.configuration.OhTeePeeConfigurations
 
 @Composable
 fun LoginPasscodeScreen(
@@ -36,20 +47,45 @@ fun LoginPasscodeScreen(
 
         Spacer(modifier = Modifier.weight(1.0f))
 
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
+        Row(
+            horizontalArrangement = Arrangement.Center
         ) {
-
-            OutlinedTextField(
-                value = passcodeText,
-                onValueChange = { passcodeText = it },
-                label = { Text(text = "Username") },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+            Spacer(modifier = Modifier.weight(1.0f))
+            OtpInput()
+            Spacer(modifier = Modifier.weight(1.0f))
         }
 
         Spacer(modifier = Modifier.weight(1.0f))
     }
+}
+
+@Composable
+fun OtpInput() {
+
+    // a mutable state to handle OTP value changes…
+    var otpValue: String by remember { mutableStateOf("") }
+
+    // this config will be used for each cell
+    val defaultCellConfig = OhTeePeeCellConfiguration.withDefaults(
+        borderColor = Color.LightGray,
+        borderWidth = 1.dp,
+        shape = RoundedCornerShape(16.dp),
+        textStyle = TextStyle(
+            color = Color.Black
+        )
+    )
+
+    OhTeePeeInput(
+        value = otpValue,
+        onValueChange = { newValue, isValid ->
+            otpValue = newValue
+        },
+        configurations = OhTeePeeConfigurations.withDefaults(
+            cellsCount = 6,
+            emptyCellConfig = defaultCellConfig,
+            cellModifier = Modifier
+                .padding(horizontal = 4.dp)
+                .size(48.dp),
+        ),
+    )
 }
